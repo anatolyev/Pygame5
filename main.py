3ae57cdf91982f1ed0a7b3800c767ec9e098fb2a
anatolyev <anatolyev@yandexlyceum.ru>
2022-12-12 16:39:58 +0300

    дописана функция загрузки изображений

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ import sys
 
 
 
-def load_image(name):
+def load_image(name, colorkey=None):
     fullname = os.path.join('images', name)
     if not os.path.isfile(fullname):
         print(f'Файл с изображением {fullname} не найден')
         sys.exit()
     image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
     return image
 
 
@@ -29,7 +36,8 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                screen.blit(image, event.pos)
+                screen.blit(image, (event.pos[0] - image.get_width()//2,
+                                    event.pos[1] - image.get_height()//2))
         # отрисовка и изменение свойств объектов
         # ...
         pygame.display.flip()
